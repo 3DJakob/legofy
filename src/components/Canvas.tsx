bd73392d0be53b38c053f11e1d6593c7e76d501a
Jakob Unnebäck <jakob@folkdatorn.se>
2022-02-14 13:14:30 +0100

    Update Canvas.tsx

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -14,6 +14,9 @@ const Container = styled.canvas`
 const Canvas: React.FC = () => {
   const inputEl = useRef<HTMLCanvasElement>(null);
   const [aspect, setAspect] = useState(1)
+  const [loaded, setLoaded] = useState(false)
+  const [numberOfLego, setNumberOfLego] = useState(40)
+  const [legoImg, setLegoImg] = useState<[string[]]>()
 
   useEffect(() => {
     const image = new Image();
@@ -33,17 +36,61 @@ const Canvas: React.FC = () => {
         inputEl.current.style.height = h + "px";
         context.scale(ratio, ratio);
 
-        context.drawImage(image, 0, 0, inputEl?.current?.width ? inputEl?.current?.width / ratio : 0, inputEl?.current?.height ? inputEl?.current?.height / ratio : 0);
+        context.drawImage(image, 0, 0, inputEl?.current?.width ? inputEl?.current?.width / ratio : 0, inputEl?.current?.height ? inputEl?.current?.height / ratio : 0)
+        setLoaded(true)
       }
     }
   })
 
+  const getColor = (x: number, y: number): Uint8ClampedArray | undefined => {
+    const context = inputEl?.current?.getContext('2d');
+    if (context != null && inputEl?.current) {
+      const p = context.getImageData(x, y, 1, 1).data; 
+      return p
+    }
+  }
+
+  const getSampleColor = (blockSize: number, xBlockIndex: number, yBlockIndex: number): string => {
+    // TODO calculate LEGO piece color
+    return '#fff'
+  }
+
+  const generateLegoImg = () => {
+    const canvasElement = inputEl?.current
+    if (canvasElement != null) {
+      // console.log(getColor(canvasElement.width -1, canvasElement.height-1))
+      const blockSize = Math.round((canvasElement.width -1) / numberOfLego)
+
+      const numberHorizontalLegos = Math.round((canvasElement.width -1) / blockSize)
+      const numberVerticalLegos = Math.round((canvasElement.height -1) / blockSize)
+
+      const myLegoImg: [string[]] = [[]]
+
+      for (let y = 0; y < numberVerticalLegos; y++) {
+        const toAdd: string[] = []
+        for (let x = 0; x < numberHorizontalLegos; x++) {
+          toAdd[x] = getSampleColor(blockSize, x, y)
+        }
+        myLegoImg[y] = toAdd
+      }
+      console.log(myLegoImg)
+    }
+  }
+
+  useEffect(() => {
+    if (loaded) {
+      generateLegoImg()
+    }
+  }, [loaded])
 
   return (
     <>
       <Container ref={inputEl} aspect={aspect}>
       </Container>
       <img src={img} width={200} style={{ marginLeft: 20 }}></img>
+      <input type="range" value={numberOfLego} onChange={e => setNumberOfLego(e?.target?.value ? Number(e.target.value) : 0)}
+         min="2" max="200"/>
+      <label >Number of LEGO pieces {numberOfLego}</label>
     </>
   )
 }
